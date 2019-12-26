54ea5e01daa33a1e428edb186d3737af01be73c8
Lee McKnight <lee_mcknight@hotmail.com>
2019-12-25 20:44:17 -0600

    initial commit

---
+++ Nero/FolderExplorer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Nero
+{
+    public class FolderExplorer : TreeView
+    {
+        public static readonly DependencyProperty FolderProperty = DependencyProperty.Register("Folder", typeof(String), typeof(FolderExplorer));
+        public String Folder
+        {
+            get { return (string)GetValue(FolderProperty);  }
+            set { SetValue(FolderProperty, value); }
+        }
+    }
+}
